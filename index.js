44f8e1f09ef6902b9c3ca5030ed734004c241d91
David Garner <dgarner@mintel.com>
2017-03-06 14:09:09 +0000

    Log and relay chat messages

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 // Entry point for server code.
 const express = require('express');
 
-const PORT = 3000;
-
 // Set up an Express app.
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const PORT = 8080;
 
 // Serve static files at the root path.
 app.use('/', express.static('static'));
 
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+  socket.on('disconnect', function () {
+    console.log('User disconnected')
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('news', { message: msg });
+  });
+});
+
 // Start the app.
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log('Listening on port ' + PORT)
 });
--- src_client/index.js
+++ src_client/index.js
@@ -1,2 +1,19 @@
 // Entry point for webpacked client code.
 document.write('Hello world!')
+
+const $ = require('jquery');
+const io = require('socket.io-client');
+
+var socket = io.connect('http://dev-reports-app04.london.mintel.ad:8080');
+socket.on('news', function (data) {
+  console.log(data);
+});
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+  $('form').submit(function() {
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+});
